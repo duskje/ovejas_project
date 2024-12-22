97ff605c32cfc33d082f00e69f3039ed3745e9eb
David Oscanoa <duskje@protonmail.com>
2024-12-22 16:03:55 -0300

    refactor: divide into modules

--- device/src/main.rs
+++ device/src/main.rs
@@ -1,7 +1,12 @@
 use std::{fs, net::TcpStream};
+use device::state::StateDelta;
 use tungstenite::{connect, stream::MaybeTlsStream, Message, WebSocket};
 
 use shared::Operation;
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
+use std::io;
 
 fn listen(socket: &mut WebSocket<MaybeTlsStream<TcpStream>>){
     let msg = socket.read().expect("Error reading message");
@@ -25,20 +30,149 @@ fn listen(socket: &mut WebSocket<MaybeTlsStream<TcpStream>>){
     }
 }
 
-fn main() {
-    env_logger::init();
+// fn main() {
+//     env_logger::init();
+// 
+//     let (mut websocket, response) = connect("ws://localhost:3000/socket").expect("Could not connect to the server");
+// 
+//     println!("Connected successfully to the server!");
+//     println!("HTTP status code: {}", response.status());
+//     println!("Response headers:");
+// 
+//     for (header, _value) in response.headers() {
+//         println!("* {header}");
+//     }
+// 
+//     loop {
+//         listen(&mut websocket);
+//     }
+// }
+//
 
-    let (mut websocket, response) = connect("ws://localhost:3000/socket").expect("Could not connect to the server");
+use std::process::Command;
 
-    println!("Connected successfully to the server!");
-    println!("HTTP status code: {}", response.status());
-    println!("Response headers:");
+use serde::{de::Error, Deserialize, Serialize};
+use serde_json::{Result, Value};
 
-    for (header, _value) in response.headers() {
-        println!("* {header}");
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    name: String,
+    gid: u32,
+    uid: u32,
+}
+
+trait ResourceActions {
+    fn create(&self);
+    fn update(&self, last_state: &Self);
+    fn delete(&self);
+}
+
+impl ResourceActions for User {
+    fn create(&self) {
+        let result = Command::new("useradd")
+            .args([
+                "--uid", self.uid.to_string().as_str(),
+                "--gid", self.gid.to_string().as_str(),
+                self.name.as_str(),
+            ])
+            .output()
+            .expect("Failed to execute process");
+
+        println!("{result:?}");
+    }
+    
+    fn update(&self, target_state: &Self) {
+        let result = Command::new("usermod")
+            .args([
+                "--uid", target_state.uid.to_string().as_str(),
+                "--gid", target_state.gid.to_string().as_str(),
+                "--login", target_state.name.to_string().as_str(),
+                self.name.as_str(),
+            ])
+            .output()
+            .expect("Failed to execute process");
     }
 
-    loop {
-        listen(&mut websocket);
+    fn delete(&self) {
+        let result = Command::new("userdel")
+            .args([self.name.as_str()])
+            .output()
+            .expect("Failed to execute process");
     }
+}
+
+// #[derive(Serialize, Deserialize, Debug)]
+// struct Resource<T> {
+//     urn: String,
+//     parameters: T,
+// }
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Resource {
+    urn: String,
+    parameters: Value,
+}
+
+
+impl Resource {
+    fn apply(&self) -> Result<()> {
+        let urn_split: Vec<&str> = self.urn.split("::").collect();
+        let [module, kind, name] = urn_split.try_into().expect("Invalid urn");
+
+        match kind {
+            "User" => {
+                let resource: User = serde_json::from_value(self.parameters.clone())?;
+                println!("{resource:?}");
+            },
+            _ => {},
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    // let target_state = r#"
+    // {
+    //   "urn": "ovejas.system::User::user_0",
+    //   "parameters": {
+    //     "name": "user0",
+    //     "uid": 500,
+    //     "gid": 100
+    //   }
+    // }"#;
+
+    // let resource: Resource = serde_json::from_str(target_state)?;
+    // println!("urn: {}", resource.parameters);
+    //
+    // resource.apply()?;
+
+    let home_dir = env::home_dir().unwrap();
+    let home_dir = home_dir.to_str().unwrap();
+    let file = File::open(format!("{home_dir}/device_info_with_res.json"));
+    //let file = File::open(format!("{home_dir}/device_info.json"));
+
+    let mut local_json = String::new();
+    let _ = file.unwrap().read_to_string(&mut local_json);
+
+    println!("Local JSON:");
+    println!("{local_json}");
+
+    let local_json: Value = serde_json::from_str(local_json.as_str())?;
+
+    let mut lines = io::stdin();
+    let mut remote_json = String::new();
+    lines.read_to_string(&mut remote_json).expect("olvidaste pipear el ejemplo");
+
+    println!("Remote JSON:");
+    println!("{remote_json}");
+
+    let remote_json: Value = serde_json::from_str(remote_json.as_str())?;
+
+    let delta = StateDelta::from_json(local_json.as_object().unwrap()["resources"].clone(), remote_json.as_object().unwrap()["resources"].clone());
+
+    println!("Delta:");
+    println!("{delta:?}");
+
+    Ok(())
 }
---
+++ device/src/state.rs
@@ -0,0 +1,67 @@
+use std::collections::{HashMap, HashSet};
+use serde_json::Value;
+
+#[derive(Debug)]
+pub struct StateDelta {
+    pub resources_to_delete: Vec<Value>,
+    pub resources_to_create: Vec<Value>,
+    pub resources_to_update: Vec<Value>,
+}
+
+impl StateDelta {
+    pub fn from_json(local_json: Value, remote_json: Value) -> Self {
+        let mut delta: Vec<&str> = Vec::new();
+        
+        let mut local_resources: HashMap<&str, Value> = HashMap::new();
+
+        for resource in local_json.as_array().unwrap() {
+            let urn = resource.get("urn").expect("Resource has no urn").as_str().expect("Could not parse str");
+            local_resources.insert(urn, resource.clone());
+        };
+
+        let mut remote_resources: HashMap<&str, Value> = HashMap::new();
+
+        for resource in remote_json.as_array().unwrap() {
+            let urn = resource.get("urn").expect("Resource has no urn").as_str().expect("Could not parse str");
+            remote_resources.insert(urn, resource.clone());
+        };
+
+        let local_keys: HashSet<&str> = remote_resources.keys().copied().collect();
+        let remote_keys: HashSet<&str> = local_resources.keys().copied().collect();
+
+        println!("{local_keys:?}");
+        println!("{remote_keys:?}");
+
+        let resources_to_create: Vec<Value> = local_keys.difference(&remote_keys)
+            .map(|local_key| remote_resources.get(local_key).unwrap())
+            .cloned()
+            .collect();
+
+        let resources_to_delete: Vec<Value> = remote_keys.difference(&local_keys)
+            .map(|remote_key| local_resources.get(remote_key).unwrap())
+            .cloned()
+            .collect();
+
+
+        let resources_to_update: Vec<Value> = remote_keys.intersection(&local_keys)
+            .map(|key| {
+                let local_resource = local_resources.get(key).unwrap();
+                let remote_resource = remote_resources.get(key).unwrap();
+
+                if  local_resource != remote_resource {
+                    Some(remote_resource)
+                } else {
+                    None
+                }
+            })
+            .filter_map(|value| value) 
+            .cloned()
+            .collect();
+
+        StateDelta {
+            resources_to_delete,
+            resources_to_create,
+            resources_to_update,
+        }
+    }
+}
